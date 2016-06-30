131acb8e24292f33e3f445815d28a25c1735d183
MigMikael <Chanachai_mig@hotmail.com>
2016-07-01 03:55:48 +0700

    Upload Picture File

---
+++ resources/views/problemfiles/index.blade.php
@@ -0,0 +1,25 @@
+@extends('template')
+@section('content')
+
+    <form action="{{route('addfile', [])}}" method="post" enctype="multipart/form-data">
+        <input type="file" name="filefield">
+        <input type="submit">
+    </form>
+
+    <h1> Pictures list</h1>
+    <div class="row">
+        <ul class="thumbnails">
+            @foreach($problemFiles as $problemFile)
+                <div class="col-md-2">
+                    <div class="thumbnail">
+                        <img src="{{route('getfile', $problemFile->filename)}}" alt="ALT NAME" class="img-responsive" />
+                        <div class="caption">
+                            <p>{{$problemFile->original_filename}}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </ul>
+    </div>
+
+@endsection
