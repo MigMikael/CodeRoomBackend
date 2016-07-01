afd2f80c6cda54a5aabaa6f369d7377003ba67de
MigMikael <Chanachai_mig@hotmail.com>
2016-07-01 17:01:57 +0700

    Communicate with Evaluator

--- resources/views/problems_analysis/create.blade.php
+++ resources/views/problems_analysis/create.blade.php
@@ -21,6 +21,21 @@
             {!! Form::text('package', null, ['class' => 'form-control', 'placeholder' => 'Your Package Name']) !!}
         </div>
         <div class="form-group">
+            {!! Form::label('enclose', 'Enclose :') !!}
+
+            {!! Form::text('enclose', null, ['class' => 'form-control', 'placeholder' => 'Parent Class']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('attribute', 'Attribute :') !!}
+
+            {!! Form::text('attribute', null, ['class' => 'form-control', 'placeholder' => 'All Attribute']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('method', 'Method :') !!}
+
+            {!! Form::text('method', null, ['class' => 'form-control', 'placeholder' => 'All Method']) !!}
+        </div>
+        <div class="form-group">
             {!! Form::label('code', 'Code :') !!}
 
             {!! Form::textarea('code', null, ['class' => 'form-control', 'placeholder' => 'Paste Your Code Here']) !!}
