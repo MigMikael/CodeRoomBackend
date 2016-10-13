76f74611db7a6fc59c91a798e6849a13d6ac49ab
Thanadej8 <authanadej@gmail.com>
2016-10-13 21:02:55 +0700

    update uploadteacher (enclose package class)

--- public/CodeRoom/js/controllers/uploadTeacherController.js
+++ public/CodeRoom/js/controllers/uploadTeacherController.js
@@ -5,6 +5,33 @@ app.controller('uploadTeacherController', ['$scope','$http','Upload','$timeout',
     $scope.isRequirment = false;
     $scope.requirment = [];
     $scope.teacherRequirement;
+    /*$scope.teacherRequirement = [
+        {
+        "id": 9,
+        "prob_id": 5,
+        "class": "public;Runners",
+        "package": "default package",
+        "enclose": "null",
+        "attribute": "1;default;int;round|2;default;float;time|",
+        "method": "1;public;void;printTest;()|",
+        "created_at": "2016-10-13 12:27:10",
+        "updated_at": "2016-10-13 12:27:10",
+        "code": ""
+        },
+        {
+        "id": 10,
+        "prob_id": 5,
+        "class": "default;Runner",
+        "package": "default package",
+        "enclose": "Runners",
+        "attribute": "1;default;int;no|2;default;int;speed|3;default;float;wasteTime|4;default;float;totalTime|",
+        "method": "1;public;int;getNo;()|2;public;void;setNo;()|3;public;int;getSpeed;()|4;public;void;setSpeed;()|5;public;float;getWasteTime;()|6;public;void;setWasteTime;()|7;public;float;getTotalTime;()|8;public;void;setTotalTime;()|",
+        "created_at": "2016-10-13 12:27:10",
+        "updated_at": "2016-10-13 12:27:10",
+        "code": ""
+        }
+    ];
+    */
 
 
     var setShow = function(){
@@ -21,6 +48,9 @@ app.controller('uploadTeacherController', ['$scope','$http','Upload','$timeout',
         for(var x in $scope.teacherRequirement){
             var objprob_id;
             var objClass = [];
+            var objEnclose = [];
+            var objPackage = [];
+            var objConstructor = [];
             var objAttribute = [];
             var objMethod = [];
             for(var y in $scope.teacherRequirement[x]){
@@ -30,28 +60,38 @@ app.controller('uploadTeacherController', ['$scope','$http','Upload','$timeout',
                 else if(y==="class"){
                     var classs = $scope.teacherRequirement[x][y].split(';');
                     //console.log("Class "+classs[0]+" "+classs[1]);
-                    objClass = [classs[0],classs[1]];
+                    objClass = ["Class",classs[0],classs[1],""];
 
 
-                }else if(y==="attribute"){
+                }
+                else if(y==="enclose") {
+                    objEnclose = ["Enclose",$scope.teacherRequirement[x][y],""];
+                }
+                else if(y==="package") {
+                    objPackage = ["Pacage",$scope.teacherRequirement[x][y],""];
+                }
+                else if(y==="constructor") {
+
+                }
+                else if(y==="attribute"){
                     var attributes = $scope.teacherRequirement[x][y].split('|');
                     for(attribute in attributes){
                         var subattributes = attributes[attribute].split(';');
                         //console.log(attributes[attribute]);
                         if(attributes[attribute]!== ""){
                            // console.log("Attribute "+subattributes[1]+" "+subattributes[2]+" "+subattributes[3]);
-                            objAttribute.push([subattributes[1],subattributes[2],subattributes[3],""]);
+                            objAttribute.push(["Attribute",subattributes[1],subattributes[2],subattributes[3],""]);
 
                         }
                     }
                 }else if(y==="method"){
-                    var methods = $scope.teacherRequirement[x][y].split('()');
+                    var methods = $scope.teacherRequirement[x][y].split('|');
                     for(method in methods){
                         var submethods = methods[method].split(';');
                         //console.log(methods[method]);
                         if(methods[method]!== ""){
                             //console.log("Method "+submethods[1]+" "+submethods[2]+" "+submethods[3]+" "+submethods[4]);
-                            objMethod.push([submethods[1],submethods[2],submethods[3],submethods[4],""]) ;
+                            objMethod.push(["Method",submethods[1],submethods[2],submethods[3],submethods[4],""]) ;
 
                         }
                     }
@@ -62,21 +102,27 @@ app.controller('uploadTeacherController', ['$scope','$http','Upload','$timeout',
                 {
                     prob_id:objprob_id,
                     class:objClass,
+                    package:objPackage,
+                    encolse:objEnclose,
+                    constructor:objConstructor,
                     attribute:objAttribute,
                     method:objMethod,
 
                 }
             );
         }
+        console.log($scope.requirment);
     };
     //upload
     $scope.uploadFiles = function(file) {
+        //setShow();
+        //showRequirement();
         
         file.upload = Upload.upload({
             url: '/problemfile/add',
             data: {filename: $scope.filename,pacekage: $scope.package, lesson_id: $scope.lesson_id,filefield: file},
         });
-
+        
         file.upload.then(function (response) {
             $timeout(function () {
                 setShow();
@@ -100,22 +146,49 @@ app.controller('uploadTeacherController', ['$scope','$http','Upload','$timeout',
         for(var i in $scope.requirment){
             for(var j in $scope.requirment[i]){
                 //console.log($scope.requirment[i][j]);
-                if(j==="attribute"){
+                if(j==="class"){
+                   if($scope.requirment[i][j][3]===""){
+                       $scope.totolPoint = $scope.totolPoint;
+
+                   }else{
+                       $scope.totolPoint += parseFloat($scope.requirment[i][j][3]);
+                   }
+                }
+                else if(j==="package"){
+                    if($scope.requirment[i][j][2]===""){
+                        $scope.totolPoint = $scope.totolPoint;
+
+                    }else{
+                        $scope.totolPoint += parseFloat($scope.requirment[i][j][2]);
+                    }
+                }
+                else if(j==="enclose"){
+                    if($scope.requirment[i][j][2]===""){
+                        $scope.totolPoint = $scope.totolPoint;
+
+                    }else{
+                        $scope.totolPoint += parseFloat($scope.requirment[i][j][2]);
+                    }
+                }
+                else if(j==="constructor"){
+
+                }
+                else if(j==="attribute"){
                     for(var z in $scope.requirment[i][j]){
                         //console.log($scope.requirment[i][j][z]);
-                        if($scope.requirment[i][j][z][3]===""){
+                        if($scope.requirment[i][j][z][4]===""){
                             $scope.totolPoint = $scope.totolPoint;
                         }else {
-                            $scope.totolPoint += parseFloat($scope.requirment[i][j][z][3]);
+                            $scope.totolPoint += parseFloat($scope.requirment[i][j][z][4]);
                         }
 
                     }
                 }else if(j==="method"){
                     for(var z in $scope.requirment[i][j]){
-                        if($scope.requirment[i][j][z][4]===""){
+                        if($scope.requirment[i][j][z][5]===""){
                             $scope.totolPoint = $scope.totolPoint;
                         }else {
-                            $scope.totolPoint += parseFloat($scope.requirment[i][j][z][4]);
+                            $scope.totolPoint += parseFloat($scope.requirment[i][j][z][5]);
                         }
 
                     }
