235b33bb102d644456d6e8d766595b7b11c13f2f
Chanachai Puttaruksa <Chanachai_mig@hotmail.com>
2016-11-03 00:16:36 +0700

    import many student to course from excel file

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Excel;
 use App\Student;
+use App\StudentCourse;
 use Request;
 use Log;
 use App\Http\Requests;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
 
 class StudentController extends Controller
 {
@@ -32,6 +36,65 @@ class StudentController extends Controller
         $student['image'] = self::getAvatarImage();
         $student = Student::create($student);
         return $student;
+    }
+
+    public function storeOneStudentMember()
+    {
+        $course_id = Request::get('course_id');
+        $student_id = Request::get('student_id');
+        $name = Request::get('name');
+
+        $student = [
+            'student_id' => $student_id,
+            'name' => $name,
+            'image' => self::getAvatarImage(),
+        ];
+        $student = Student::create($student);
+
+        $studentCourse = [
+            'student_id' => $student->id,
+            'course_id' => $course_id
+        ];
+        StudentCourse::create($studentCourse);
+
+        return 'add student complete';
+    }
+
+    public function storeManyStudentMember()
+    {
+        $course_id = Request::get('course_id');
+        $studentListFile = Request::file('studentList');
+
+        $fileName = $studentListFile->getClientOriginalName();
+        Storage::disk('public')->put($fileName, File::get($studentListFile));
+        $path = storage_path().'\\app\\public\\' . $fileName;
+        $data = Excel::load($path, function ($reader){
+
+        })->get();
+
+        $students = [];
+        if(!empty($data) && $data->count()){
+            foreach ($data as $key => $value) {
+                $students[] = ['student_id' => $value->id, 'name' => $value->name];
+                //Log::info('###### '. $value->student_id.' '.$value->name);
+            }
+            if(empty($students)){
+                return 'error in data File';
+            }
+        }
+
+        foreach ($students as $student){
+            $student['image'] = self::getAvatarImage();
+            $student = Student::create($student);
+
+            $studentCourse = [
+                'student_id' => $student->id,
+                'course_id' => $course_id
+            ];
+            StudentCourse::create($studentCourse);
+        }
+
+        return 'add student complete';
     }
 
     public function getAvatarImage()
--- resources/views/course/member.blade.php
+++ resources/views/course/member.blade.php
@@ -6,7 +6,30 @@
             <h1>Add Student Member</h1>
         </div>
         <br>
-        {!! Form::open(['url' => 'api/course/add_student_member', 'files' => true]) !!}
+        {!! Form::open(['url' => 'api/student/add_one_student_member']) !!}
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                {!! Form::label('course_id', 'CourseID :', ['class' => 'mdl-textfield__label']) !!}
+                {!! Form::text('course_id', null, ['class' => 'mdl-textfield__input']) !!}
+            </div>
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                {!! Form::label('student_id', 'StudentID :', ['class' => 'mdl-textfield__label']) !!}
+                {!! Form::text('student_id', null, ['class' => 'mdl-textfield__input']) !!}
+            </div>
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                {!! Form::label('name', 'Name :', ['class' => 'mdl-textfield__label']) !!}
+                {!! Form::text('name', null, ['class' => 'mdl-textfield__input']) !!}
+            </div>
+            <br>
+            {!! Form::submit('add',['class' => 'mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent']) !!}
+        {!! Form::close() !!}
+    </div>
+
+    <div class="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--2dp big-card">
+        <div class="center">
+            <h1>Add Student Member</h1>
+        </div>
+        <br>
+        {!! Form::open(['url' => 'api/student/add_many_student_member', 'files' => true]) !!}
             <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                 {!! Form::label('course_id', 'CourseID :', ['class' => 'mdl-textfield__label']) !!}
                 {!! Form::text('course_id', null, ['class' => 'mdl-textfield__input']) !!}
