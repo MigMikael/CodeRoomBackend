7aabeac953035da924c24106bd2f411c61c4200e
Chanachai Puttaruksa <Chanachai_mig@hotmail.com>
2016-11-07 19:54:20 +0700

    add course status

--- database/seeds/CourseTableSeeder.php
+++ database/seeds/CourseTableSeeder.php
@@ -16,6 +16,7 @@ class CourseTableSeeder extends Seeder
             'name' => 'Computer Programming I',
             'image' => 'http://localhost:8000/api/course/image/'. str_replace('.','_','c.png'),
             'color' => '244:67:54',
+            'status' => 'active',
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
@@ -24,6 +25,7 @@ class CourseTableSeeder extends Seeder
             'name' => 'Computer Programming II',
             'image' => 'http://localhost:8000/api/course/image/'. str_replace('.','_','java.png'),
             'color' => '239:108:0',
+            'status' => 'active',
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
@@ -32,6 +34,7 @@ class CourseTableSeeder extends Seeder
             'name' => 'Data Structures',
             'image' => 'http://localhost:8000/api/course/image/'. str_replace('.','_','datastruct.png'),
             'color' => '0:131:143',
+            'status' => 'active',
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
@@ -40,6 +43,16 @@ class CourseTableSeeder extends Seeder
             'name' => 'Object Oriented Software Development',
             'image' => 'http://localhost:8000/api/course/image/'. str_replace('.','_','oosd.png'),
             'color' => '63:81:181',
+            'status' => 'active',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+
+        DB::table('course')->insert([
+            'name' => 'Research Method',
+            'image' => 'http://localhost:8000/api/course/image/'. str_replace('.','_','rm.png'),
+            'color' => '0:105:92',
+            'status' => 'inactive',
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
